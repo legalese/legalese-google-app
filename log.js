0dbf742513982c95ad61baa73b0d1b66dafa1a2e
Anuj Gupta <code@anuj.me>
2016-09-19 14:11:04 +0530

    Very minor refactoring - Whitespace, DRY

--- log.js
+++ log.js
@@ -146,6 +146,7 @@ function teLog(params, loglevel, logconfig) {
   }
   myLog(params,"templates.js", loglevel, logconfig);
 }
+
 function utLog(params, loglevel, logconfig) {
   if (params.constructor.name != "Array") { // allow backward compatibility
 	params = Array.prototype.slice.call(arguments); loglevel = null; logconfig = null;
@@ -157,7 +158,6 @@ function xmLog(params, loglevel, logconfig) {
   if (params.constructor.name != "Array") { // allow backward compatibility
     params = Array.prototype.slice.call(arguments); loglevel = null; logconfig = null;
   }
-  if (loglevel == undefined) { loglevel = 7 }
   myLog(params, "(XML) " + currentTemplate, loglevel, logconfig);
 }
 
