15afd9ec1ea40e98788bc0fc2f73d42bf3a6e67c
meng wong <mengwong@pobox.com>
2016-10-01 18:04:51 -0400

    add missing log.js
    this was supposed to be part of a pull request but seems to have been omitted.

---
+++ log.js
@@ -0,0 +1,24 @@
+function xmLog(params, loglevel, logconfig) {
+  if (params.constructor.name != "Array") { // allow backward compatibility
+    params = Array.prototype.slice.call(arguments); loglevel = null; logconfig = null;
+  }
+  if (loglevel == undefined) { loglevel = 7 }
+  myLog(params, "xml", loglevel, logconfig);
+}
+
+var jsonify = function(obj){
+    var seen = {};
+    var counter = 0;
+    var json = JSON.stringify(obj, function(key, value){
+        if (typeof value === 'object') {
+            if ( value != null && seen.hasOwnProperty(value) ) {
+                return '__cycle__' + (typeof value) + '[' + key + '(' + seen[value] + ')]'; 
+            }
+            seen[value] = counter;
+            counter++;
+            Logger.log("seen["+counter+"] is the value of %s", key);
+        }
+        return value;
+    }, 4);
+    return json;
+};
