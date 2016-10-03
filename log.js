6ba284b933bc46f7218270b122ab44fd2e91d5be
Anuj Gupta <code@anuj.me>
2016-10-03 16:27:37 +0530

    Moved Meng's jsonify() from log.js to util.js

--- log.js
+++ log.js
@@ -161,19 +161,3 @@ function xmLog(params, loglevel, logconfig) {
   myLog(params, "(XML) " + currentTemplate, loglevel, logconfig);
 }
 
-var jsonify = function(obj){
-    var seen = {};
-    var counter = 0;
-    var json = JSON.stringify(obj, function(key, value){
-        if (typeof value === 'object') {
-            if ( value != null && seen.hasOwnProperty(value) ) {
-                return '__cycle__' + (typeof value) + '[' + key + '(' + seen[value] + ')]'; 
-            }
-            seen[value] = counter;
-            counter++;
-            Logger.log("seen["+counter+"] is the value of %s", key);
-        }
-        return value;
-    }, 4);
-    return json;
-};
