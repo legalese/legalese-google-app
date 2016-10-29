a1d6cb850ab010e970714bf079ccd15b234f482a
meng wong <mengwong@pobox.com>
2016-10-29 17:07:31 -0400

    start thinking about ESOP holder refactoring

--- esop.js
+++ esop.js
@@ -20,13 +20,15 @@ function ESOP_(security_type, initial_num_shares) {
   this.security_type = security_type;
   this.initial_num_shares = initial_num_shares;
 
-  this.holders = {}; // { investor name : num esop shares }
+  this.holders = {};
+  // { investor name : ESOPHolder_ }
 
-  this.createHolder = function(name) { this.holders[name] = this.holders[name] || 0 };
+  this.createHolder = function(name) { this.holders[name] = this.holders[name] || new ESOPHolder_(this, name); return this.getHolder(name) };
+  this.getHolder    = function(name) { return this.holders[name] };
   this.deleteHolder = function(name) { delete this.holders[name] };
-  this.holderGains  = function(name, num) { this.holders[name] += num };
-  this.holderLoses  = function(name, num) { this.holders[name] -= num };
-
+  this.holderGains  = function(name, num, type) { this.holders[name].gain(num, type) };
+  this.holderLoses  = function(name, num, type) { this.holders[name].lose(num, type) };
+  
   this.total     = function() { return this.initial_num_shares + this.holders["ESOP"] };
   this.reserved  = function() { return this.holders["ESOP"] };
   this.issued    = function() {
@@ -39,9 +41,29 @@ function ESOP_(security_type, initial_num_shares) {
 
   // TODO: at any given round the ESOP should be able to tell us how many restricted and unrestricted shares there are
   // based on the periodic vesting data available under ROLES in the termsheet in which those shares were first allotted.
-  this.dynamicTotal = function () { }
+  this.dynamicTotal = function () { };
   
   
   esLog("instantiating ESOP object. security_type=%s, initial_num_shares=%s", security_type, initial_num_shares);
 }
 
+function ESOPHolder_(esopParent, holderName) {
+  this.esop = esopParent;
+  this.name = holderName;
+
+  this.set = function(key, value) { this[key] = value; return this.set; };
+  this.get = function(key)        { return this[key]; }
+  
+  // { esop_start_date: Date,
+  //   initial_f_restricted: Int,         // initial allocation at time ESOP was set up
+  //   initial_f_unrestricted: Int,       // "
+  //   subsequent_f_restricted: Int,      // aggregate of all adjustments made subsequently
+  //   subsequent_f_unrestricted: Int,    // "
+  //   cliff: Int,
+  //   cliff_end_date: Date,
+  //   cliff_2: Int,
+  //   cliff_end_date: Date,
+  //   periodic: Int,
+  //   period: DateInterval, // default Monthly
+  // }
+}
