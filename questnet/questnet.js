b5c32a4b3d79e17c65427022ca4b768897b252ce
Job Chong <jobchong@gmail.com>
2017-01-02 21:42:32 +0800

    some cleanup

--- questnet/questnet.js
+++ questnet/questnet.js
@@ -13,8 +13,8 @@ casper.start('https://www.questnet.sg/Maincontent.asp', function() {
 casper.then(function() {
     casper.waitForSelector("form input[name='txtUserID']", function() {
 	this.fillSelectors('form', {
-            'input[name = txtUserID ]' : 'username',
-            'input[name = txtPassword ]' : 'password'
+            'input[name = txtUserID ]' : casper.cli.args[0],
+            'input[name = txtPassword ]' : casper.cli.args[1]
 	}, true);
 	this.echo("ive filled in the fields");
     });
@@ -33,13 +33,6 @@ casper.then(function() {
 casper.then(function() {
     this.mouse.click(100, 500);
     this.echo("removed overlay");
-/*    
-      if (this.exists("td.searchLink1")) {
-	this.echo("am i in the right frame?");
-    } else {
-	this.echo("no i'm not, good")
-    }
-*/
 });
 
 // switch to child frame. frame focus only lasts in scope
@@ -54,39 +47,40 @@ casper.withFrame("main", function() {
 // input search term
 
 casper.withFrame("main", function() {
-    this.sendKeys("input[class = uiCompanyName]", "apple"); // this should be changed to UEN soon because we don't to retrieve all string matches and pay for each retrieval
-    if (this.exists("td#BrowseButton")) {
-	this.echo("i can see the search button");
-    } else {
-	this.echo('dead');
-    }
-    this.sendKeys("input[class = uiCompanyName]", casper.page.event.key.Enter);
+    this.sendKeys("input[class = uiCompanyRegno]", casper.cli.args[2]); // this should be changed to UEN soon because we don't to retrieve all string matches and pay for each retrieval
+    this.mouse.click(250, 425);
     this.echo("i have clicked");
     // can't access the frame, have to wait the dumb way
-    this.wait(2000, function() {
+    this.wait(1000, function() {
 	this.mouse.click(496, 251);
 	this.page.sendEvent("keypress", casper.page.event.key.Enter);
 	this.echo("done");
     });
-    this.wait(2000, function() {
+    this.wait(1000, function() {
 	this.mouse.click(303, 495);
 	this.mouse.click(750, 560); // this is for mastercard; after submitting request to finance channel should be able to click on the prepay option
+ });
+});
+
+// nets payment is unfeasible because of 2FA, but included here for completeness.
+
+casper.then(function() {
+    this.sendKeys('input[name=name]', 'name');
+    this.sendKeys('input[name=cardNo]', 'card number');
+    this.sendKeys('input[name=cvv]', 'cvv');
+    this.evaluate(function() {
+        document.querySelector('select[name=expiryMonth]').selectedIndex = 1; // numerical value of the month of expiry
     });
+    this.sendKeys('input[name=expiryYear]', 'year of expiry');
+    this.click('input[name=agree]');
+    // this sends for payment, so commented out
+    // this.click('input#submit');
 });
 
-// nets payment (not currently working atm)
-
-/*
-casper.wait(3000, function() {
-    if (this.exists("table:nth-child(2)")) {
-	this.echo("hi");
-    };
-    if (this.exists("input[name=name]")) {
-    	this.echo("here");
-    };
+casper.then(function() {
+    this.capture('results.png');
+    this.echo('ive just spent five dollars');
 });
-*/
-
 
 // sometimes search has been made in the last 5 days; questnet will redirect to currently existing order to avoid repayment
 
@@ -106,7 +100,7 @@ casper.wait(2000, function() {
 casper.withFrame("top", function() {
     this.clickLabel("COLLECT ORDERS");
     this.waitForSelector("td[class=dptxtblue2bold]", function() {
-	this.echo("here");;
+ 	this.echo("here");;
     });
 });
 
@@ -117,7 +111,7 @@ casper.withFrame("listFrame", function() {
 
 casper.withFrame("contentFrame", function() {
     this.waitForSelector("td[class=titleFont1]", function() {
-	this.echo("loaded");
+ 	this.echo("loaded");
     });
 });
 
@@ -125,7 +119,7 @@ casper.withFrame("contentFrame", function() {
 
 casper.withFrame("contentFrame", function() {
     var info = this.evaluate(function() {
-	var nodes = document.querySelectorAll("td[class=DtaFld]");
+ 	var nodes = document.querySelectorAll("td[class=DtaFld]");
     });
     require('fs').write("results.json", JSON.stringify(info), "w"); // this is messy, will deal with it on the node side
 });
