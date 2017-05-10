4bcb4bb1b1c792a5091b04cef10309b98383a79d
Job Chong <jobchong@gmail.com>
2017-05-10 15:44:04 +0800

    complete script for adding test library

---
+++ tests/scripts/README.md
@@ -0,0 +1,18 @@
+# What does addLibrary.js do?
+
+Navigates to the canonical test spreadsheet's attached script, removes the current legaleseMain library and adds your test library for testing a change/patch etc.
+
+## How do I use this?
+
+Install [casperjs](http://casperjs.org/) and [slimerjs](https://slimerjs.org/).
+Then: `casperjs --engine=slimerjs <email> <password> <your library's identifier>`
+
+### Where do I get my library's identifier?
+
+When you're at your library's script editor page: `Files/Project Properties`.
+
+## What do I need to do before using this script/common gotchas
+
+At your library: `File/Manage Versions/Save New Version`. Do this after every change you want to test.
+Ensure that the email account you are using for this script is the same as the one you used when creating the test library, otherwise Google spits permissions errors.
+Ensure you also have write access to the test spreadsheet/script.
--- tests/scripts/addLibrary.js
+++ tests/scripts/addLibrary.js
@@ -5,21 +5,29 @@ casper.options.viewportSize = {width: 1920, height: 1080};
 var mouse = require("mouse").create(casper);
 var fs = require('fs');
 
-casper.start('https://script.google.com/macros/d/M2vn1ETAPoOIrHOWyJkTVroD70ir8sWkZ/edit?uiv=2&mid=ACjPJvGZMNGWqRO5TqKrVU8COn2KacXWK814Tsx23VQzMQw4tYlfXdmGJI3GTogzzivms4V4US8I0UbKP0tSRcD1iTHffy6wDQE5U06dwQ6A3mliOdV_BJoMsOVnkfrFydLd58m9Ho0k2gQ', function() {
+// left blank before selection of official test sheet; will be filled in with the attached script URL
+
+casper.start('', function() {
     this.echo('at test sheet script editor');
 });
 
+// get email
+
 casper.then(function() {
-    this.sendKeys('input#Email', '');
+    this.sendKeys('input#Email', casper.cli.args[0]);
 });
+
+// get password
 
 casper.then(function() {
     this.click('input#next');
     this.waitForSelector('#Passwd', function() {
-	this.sendKeys('input#Passwd', '');
+	this.sendKeys('input#Passwd', casper.cli.args[1]);
 	this.echo('pwd');
     });
 });
+
+// click signin
 
 casper.then(function() {
     this.waitForSelector('#signIn', function() {
@@ -28,18 +36,57 @@ casper.then(function() {
     });
 });
 
+// click resources/libraries
+
 casper.then(function() {
     this.waitForSelector('div.gwt-Label.name', function() {
-	this.click('div#macros-resources-menu');
+ 	this.click('div#macros-resources-menu');
+ 	this.mouse.click(474, 80); // click 'Libraries...'
     });
 });
 
+// delete original legaleseMain
+
 casper.then(function() {
-    this.click('div#\\:1q');
-    this.echo('clicked');
-    this.sendKeys('input.gwt-TextBox.textbox', 'MBFocfLAJmuH5tvP1xdfvRaQ2d-IWEG6q');
-    this.click('button.gwt-Button.button');
-    this.capture('sheet.png');
+    this.waitForSelector('div.maestro-dialog', function() {
+ 	this.echo('hi');
+ 	this.waitForSelector('input.gwt-TextBox.textbox', function() {
+ 	    this.mouse.click(1225, 385); // click delete
+ 	    this.mouse.click(697, 648); // click save
+ 	    this.echo('deleted orig lib');
+ 	});
+    });
 });
+
+// click resources/libraries
+
+casper.then(function() {
+    this.waitForSelector('div.gwt-Label.name', function() {
+	this.click('div#macros-resources-menu');
+	this.mouse.click(474, 80); // click 'Libraries...'
+    });
+});
+
+// add library to be tested
+
+casper.then(function() {
+    this.waitForSelector('div.maestro-dialog', function() {
+	this.waitForSelector('input.gwt-TextBox.textbox', function() {
+	    this.sendKeys('input.gwt-TextBox.textbox', casper.cli.args[2]), // get your library identifier from File/Manage Versions
+	    this.click('button.gwt-Button.button');
+	    this.waitForSelector('input.gwt-TextBox.identifier.item', function() {
+		this.sendKeys('input.gwt-TextBox.identifier.item', 'legaleseMain', { reset: true });
+		this.click('div.goog-button-base-content');
+		this.waitForSelector('tr.version', function() {
+		    this.mouse.click(861, 452); // click most recent library version - assumes that's the one to be tested
+		    this.mouse.click(1136, 385); // toggle dev-mode
+		    this.mouse.click(697, 648); // click save
+		});
+	    });
+	});
+    });
+});
+
+
 
 casper.run();
