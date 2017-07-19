4a38b9b019c5ea6f89e8f38c9ed1b76b5d3dcd31
Job Chong <jobchong@gmail.com>
2017-07-19 22:31:58 +0800

    now with linebreaks

--- questnet/fiddling.js
+++ questnet/fiddling.js
@@ -3,13 +3,13 @@ var casper = require('casper').create();
 var mouse = require("mouse").create(casper);
 var fs = require('fs');
 
-// usage: casperjs fiddling.js file://localhost`pwd`/examples/facebook.html
+// usage: casperjs fiddling.js file://localhost`pwd`/examples/facebook.html output.json
 
 // navigate straight to relevant asp page
 
 var startUrl = (casper.cli.args.length > 0)
-    ? casper.cli.args[0]
-    : 'https://www.questnet.sg/Maincontent.asp';
+	     ? casper.cli.args[0]
+	     : 'https://www.questnet.sg/Maincontent.asp';
 
 casper.start(startUrl, function() { // INSERT YOUR LOCAL FILE HERE
     this.echo(startUrl + " loaded");
@@ -17,37 +17,33 @@ casper.start(startUrl, function() { // INSERT YOUR LOCAL FILE HERE
 
 casper.then(function() {
 
-	// make dom elements available to casperjs
-	
-	var keyArr = [];
-	var valArr = [];	
+    // make dom elements available to casperjs
+    
+    var keyArr = [];
+    var valArr = [];	
     var htmlArr = this.getElementsInfo('td.DtaFld').map(function(e) { return e.html } );
     var nricArr = this.evaluate(function() {
         var elements = __utils__.findAll('td.DtaFld a.searchlink');
         return elements.map(function(e) {
-          return e.text;
+            return e.text;
         });
     });
-  
-	var elements = this.getElementsInfo('td.lblFld');
+    
+    var elements = this.getElementsInfo('td.lblFld');
     for (var i = 0; i < elements.length; i++) {
-	    keyArr.push(elements[i].text);
-	}
-	var val = this.getElementsInfo('td.DtaFld');
-	for (var i = 0; i < val.length; i++) {
-	    valArr.push(val[i].text);
-	}
+	keyArr.push(elements[i].text);
+    }
+    var val = this.getElementsInfo('td.DtaFld');
+    for (var i = 0; i < val.length; i++) {
+	valArr.push(val[i].text);
+    }
 
-	this.echo(keyArr);
-    this.echo(valArr);
-    this.echo(htmlArr);
-    this.echo(nricArr);
-	this.echo('Getting details');
-	var info = initialInfo(keyArr, valArr, nricArr);
-	
-    fs.write('results.json', JSON.stringify(info, null, 2), 'w');
-    fs.write('html.json', JSON.stringify(htmlArr, null, 2), 'w');
-	this.echo('Done!');
+    this.echo('Getting details');
+    var info = initialInfo(keyArr, htmlArr, nricArr);
+    
+    fs.write(casper.cli.args[1], JSON.stringify(info, null, 2), 'w');
+    
+    this.echo('Done!');
 });
 
 casper.run();
@@ -56,26 +52,33 @@ casper.run();
 // parser constructor functions
 
 function initialInfo(keys, values, ic) {
+    
     var result = {};
 
+    var htmlId = 1; // counter for the ID <a.searchlink> array
+ 
     // we get the initial general information first
 
     for (var i = 0; i < keys.length; i++) {
 	if (keys[i] == 'Capital Structure:') {
-	    break; 
+	    break;
 	};
-	result[keys[i]] = values[i].replace(/[\n\t]/g, '');
+	if (i == 3) {
+	    result[keys[i]] = ic[0]; // company UEN from the ID array
+	    continue;
+	}
+	result[keys[i]] = values[i].replace(/\t/g, '')
     };
 
     result.capStructure = capStructure(keys, values);
-    result.directors = getDirectors(values, ic);
-    result.shareholders = getShareholders(values);
+    result.directors = getDirectors(values, ic, htmlId);
+    result.shareholders = getShareholders(values, ic, htmlId);
     return result;
 };
 
 function capStructure(keys, values) {
     for (var i = 0; i < keys.length; i++) {
-	if (keys[i] == 'Capital Structure') {
+	if (keys[i] == 'Capital Structure:') {
 	    var shares = {
 		issuedOrdinary: {
 		    shares: values[i+1],
@@ -95,42 +98,31 @@ function capStructure(keys, values) {
 
 // directors have four table fields
 
-function addresses(addressField) {
-    var re = /\n\t\t\t\t\t/g;
-    var match, indexes = [];
-    while ((match = re.exec(addressField)) != null) {
-	indexes.push(match.index);
-	console.log(match.index);
-    };
-    return indexes;
-}
 
-function getDirectors(values, ic) {
+function getDirectors(values, ic, id) {
     var directors = [];
 
-    var idReg = /[A-Za-z]\d{7}[A-za-z]|\d{9}[A-za-z]/; // matches ICs and UENs
+    var idReg = /[A-Za-z]\d{7}[A-za-z]|\d{9}[A-za-z]|[A-za-z]{2}\d{7}|\d{9}/; // matches ICs and UENs
 
-    var tabReg = /\n\t\t\t\t\t/g;
     for (var i = 0; i < values.length; i++) {
 
 	// does the previous cell have a date/is empty and does the next cell contain an IC or UEN? If so, this is the start of the director's block
 	
 	if (!(/[A-Za-z]/.test(values[i])) && idReg.test(values[i+1])) {
 	    
-	    var htmlId = 1;
 	    for (var j = i + 1; j < values.length; j += 4) {
 		if (values[j+3] == 'ORDINARY' || values[j+3] == 'PREFERENCE') {
 		    break;
 		};
 		var newDirector = {
-		    nameID: values[j].slice(0, tabReg.exec(values[j]).index),
-		    id: ic[htmlId],
-		    address: values[j+1].slice(0, addresses(values[j+1])[2]).replace(tabReg, ' '),
+		    nameID: values[j].slice(0, /<br>/g.exec(values[j]).index),
+		    id: ic[id],
+		    address: values[j+1].slice(0, addresses(values[j+1])[2]),
 		    nationality: values[j+2],
-		    appointment: values[j+3].slice(10).replace(tabReg, ' ') // cut off dates
+		    appointment: values[j+3].slice(10).replace(re, ' ') // cut off dates
 		};
 		directors.push(newDirector);
-		htmlId++;
+		id++;
 	    }
 	}
     }
@@ -139,12 +131,10 @@ function getDirectors(values, ic) {
 
 // shareholders have six table cells
 
-function getShareholders(values) {
-    var shareholders = [];
-    var idReg = /[A-Za-z]\d{7}[A-za-z]|\d{9}[A-za-z]/;
-    var countryReg = /singapore|united states|malaysia/i;
-    var postalReg = /\d{6}/;
+function getShareholders(values, ic, id) {
     
+    var shareholders = [];
+
     for (var i = 0; i < values.length; i++) {
 
 	// check start and end of shareholder block
@@ -154,26 +144,32 @@ function getShareholders(values) {
 	    for (var j = i; j < values.length; j += 6) {
 		if (values[j+3] == undefined) {
 		    break;
-		};
-
-		var id = idReg.exec(values[j]);
-		var country = countryReg.exec(values[j+2]);
-		var postal = postalReg.exec(values[j+2]);
-		
+		}
 		var newShareholder = {
-		    nameID: values[j].slice(0, id.index),
-		    id: values[j].slice(id.index),
+		    nameID: values[j].slice(0, /<br>/g.exec(values[j]).index),
+		    id: ic[id],
 		    nationality: values[j+1],
-		    address: values[j+2].slice(0, country.index).replace('#', ' #'),
-		    postalCode: values[j+2].slice(country.index, postal.index + 6),
+		    address: values[j+2].slice(0, addresses(values[j+2])[2]).replace(/\n\t\t\t\t\t\t\t\t\t\t\t\t\t/g, ''),
 		    sharetype: values[j+3],
 		    sharenumber: values[j+4],
 		    currency: values[j+5]
 		}
 		shareholders.push(newShareholder);
+		id++;
 	    }
 	    break;
 	}
     }
     return shareholders;
+}
+
+// cut addresses
+
+function addresses(addressField) {
+    var re = /<br>/g;
+    var match, indexes = [];
+    while ((match = re.exec(addressField)) != null) {
+	indexes.push(match.index);
+    };
+    return indexes;
 }
