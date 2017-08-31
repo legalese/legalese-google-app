411ffaf7121183549a9d3cd7568968c8c9cb55f9
sukits <sukit.s@proteus-tech.com>
2017-08-31 12:35:26 +0700

    modified questnet.js

--- questnet/questnet.js
+++ questnet/questnet.js
@@ -2,17 +2,21 @@ var casper = require('casper').create();
 var mouse = require("mouse").create(casper);
 var fs = require('fs');
 
-var start = Date.now();
-
 // navigate straight to relevant asp page
 
 casper.start('https://www.questnet.sg/Maincontent.asp', function() {
+    //this.echo("Main page loaded");
+});
+
+// select form and input login details
+
+casper.then(function() {
     casper.waitForSelector("form input[name='txtUserID']", function() {
-	this.fillSelectors('form', {
-            'input[name = txtUserID ]' : casper.cli.args[0],
-            'input[name = txtPassword ]' : casper.cli.args[1]
-	}, true);
-	this.echo("Login details filled");
+    	this.fillSelectors('form', {
+                'input[name = txtUserID ]' : casper.cli.args[0],
+                'input[name = txtPassword ]' : casper.cli.args[1]
+    	}, true);
+	//this.echo("Login details filled");
     });
 });
 
@@ -20,113 +24,157 @@ casper.start('https://www.questnet.sg/Maincontent.asp', function() {
 
 casper.then(function() {
     this.mouse.click("form input[value=LOGIN]");
-    this.echo("Logging in...");
+    //this.echo("Logging in...");
 
 });
 
 casper.then(function() {
-    this.mouse.click(100, 200);
-    this.echo("Removed maintenance overlay");
+    this.mouse.click(100, 500);
+    //this.echo("Removed maintenance overlay");
 });
 
 // switch to child frame. frame focus only lasts in scope
 
-casper.withFrame("main", function() {
-    this.clickLabel("search");
+casper.then(function() {
+	this.waitForSelector('frame[name=main]', function() {
+		casper.withFrame("main", function() {
+			if (this.exists("td.searchLink1")) {
+			//this.echo("Checking for the right frame");
+			}
+			this.clickLabel("search");
+		});
+	});
 });
 
 // input search term
+casper.then(function() {
+	this.waitForSelector('frame[name=main]', function() {
+		casper.withFrame("main", function() {
+			this.sendKeys("input[class = uiCompanyRegno]", casper.cli.args[2]);
 
-casper.withFrame("main", function() {
-    this.sendKeys("input[class = uiCompanyRegno]", casper.cli.args[2]);
-    this.clickLabel('Browse');
-    this.echo('Search submitted');
+			// alert shows if UEN is invalid
+
+			casper.waitForAlert(function then() {
+			     this.die('UEN was entered in an invalid format! Try again.')
+			}, function timeout() {
+			//this.echo('UEN valid, conducting search');
+			});
+			this.clickLabel('Browse');
+			//this.echo('Search submitted');
+		});
+	});
 });
 
 // whole bunch of callbacks because all these elements are loaded by js
 
-casper.withFrame('main', function() {
-    this.waitForSelector('div.content', function() {
-	this.waitForSelector('iframe.GB_frame', function() {
-	    casper.withFrame('GB_frame', function() {
-		this.waitForSelector('iframe#GB_frame', function() {
-		    casper.withFrame(0, function() {
-			this.waitForSelector('select#lstIDName', function() { // actual form
-			    this.evaluate(function() {
-				document.querySelector('select#lstIDName').selectedIndex = 0; // assume only 1 result of search by uen
-			    });
-			    this.click('td.button_bluesmall > img:first-child');
-			    this.echo('Result selected');
+casper.then(function() {
+	this.waitForSelector('frame[name=main]', function() {
+		casper.withFrame('main', function() {
+			this.waitForSelector('div.content', function() {
+			this.waitForSelector('iframe[name=GB_frame]', function() {
+				casper.evaluate(function() {
+					document.querySelector('iframe[name=GB_frame]').contentWindow.document.querySelector('iframe#GB_frame').contentWindow.document.querySelector('select#lstIDName').selectedIndex = 0;
+					document.querySelector('iframe[name=GB_frame]').contentWindow.document.querySelector('iframe#GB_frame').contentWindow.document.querySelector('td.button_bluesmall img:first-child').click();
+				});
+				//this.echo('Result selected');
 			});
-		    });
+			});
 		});
-	    });
 	});
-    });
 });
 
-casper.withFrame('main', function() {
-    this.click('a#IdSubmit'); // point button
-    this.echo('Clicked pay');
+casper.then(function() {
+	this.waitForSelector('frame[name=main]', function() {
+		casper.withFrame('main', function() {
+			this.click('a#IdSubmit'); // point button
+			//this.echo('Clicked pay');
+
+			//if search returns no results
+
+			casper.waitForAlert(
+				function() {
+					this.die('Search has returned no results! Exiting script.');
+				},
+				function() {
+					//this.echo('Search has returned 1 or more relevant results. Selected.');
+			});
+		});
+	});
 });
 
 // repeat orders don't display the collect order button for some reason, so we navigate twice
-
-casper.withFrame('top', function() {
-    this.page.sendEvent('keypress', casper.page.event.key.Enter);
-    this.clickLabel('SEARCH MENU');
+casper.then(function() {
+	this.waitForSelector('frame[name=top]', function() {
+		casper.withFrame('top', function() {
+			this.page.sendEvent('keypress', casper.page.event.key.Enter);
+			this.clickLabel('SEARCH MENU');
+			//this.echo('Clicked SEARCH MENU');
+		});
+	});
 });
 
-casper.withFrame('top', function() {
-    this.clickLabel('COLLECT ORDERS');
+casper.then(function() {
+	this.waitForSelector('frame[name=top]', function() {
+		casper.withFrame('top', function() {
+			this.clickLabel('COLLECT ORDERS');
+			//this.echo('Clicked COLLECT ORDERS');
+		});
+	});
 });
 
 // search for the correct order item from the UEN entered previously
 
-casper.withFrame("main", function() {
-    casper.withFrame('listFrame', function() {
-	var re = new RegExp(casper.cli.args[2], 'gi');
-	var htmlArr = this.getElementsInfo('td.CF').map(function(e) { return e.html } );
-	for (var i = 0; i < htmlArr.length; i++) {
-	    if (re.test(htmlArr[i])) {
-		var labelLength = htmlArr[i-1].length; // cell with order item link is one cell before cell containing the name/uen of search result
-		var label = htmlArr[i-1].slice(labelLength - 12, labelLength - 4); // get order no.
-		this.echo('Found the correct order item');
-		this.clickLabel(label);
-		this.echo('Clicked on the order item');
-	    }
-	}
-    });
+casper.then(function() {
+	this.waitForSelector('frame[name=main]', function() {
+		casper.withFrame('main', function() {
+			casper.withFrame('listFrame', function() {
+			var re = new RegExp(casper.cli.args[2], 'gi');
+			var htmlArr = this.getElementsInfo('td.CF').map(function(e) { return e.html } );
+			for (var i = 0; i < htmlArr.length; i++) {
+				if (re.test(htmlArr[i])) {
+				var labelLength = htmlArr[i-1].length; // cell with order item link is one cell before cell containing the name/uen of search result
+				var label = htmlArr[i-1].slice(labelLength - 12, labelLength - 4); // get order no.
+				//this.echo('Found the correct order item');
+				this.clickLabel(label);
+				//this.echo('Clicked on the order item');
+				}
+			}
+			});
+		});
+	});
 });
 
-casper.withFrame('main', function() {
-    casper.withFrame('contentFrame', function() {
+casper.then(function() {
+	this.waitForSelector('frame[name=main]', function() {
+	casper.withFrame('main', function() {
+		casper.withFrame('contentFrame', function() {
 
-	// make dom elements available to casperjs
-	
-	var keyArr = [];
-	var valArr = [];	
-	var htmlArr = this.getElementsInfo('td.DtaFld').map(function(e) { return e.html } );
-	var nricArr = this.evaluate(function() {
-	    var elements = __utils__.findAll('td.DtaFld a.searchlink');
-	    return elements.map(function(e) {
-		return e.text;
-	    });
+		// make dom elements available to casperjs
+
+		var keyArr = [];
+		var valArr = [];
+		var htmlArr = this.getElementsInfo('td.DtaFld').map(function(e) { return e.html } );
+		var nricArr = this.evaluate(function() {
+			var elements = __utils__.findAll('td.DtaFld a.searchlink');
+			return elements.map(function(e) {
+			return e.text;
+			});
+		});
+
+		var elements = this.getElementsInfo('td.lblFld');
+		for (var i = 0; i < elements.length; i++) {
+			keyArr.push(elements[i].text);
+		}
+
+		//this.echo('Getting details');
+		var info = initialInfo(keyArr, htmlArr, nricArr);
+
+		//fs.write('results.json', JSON.stringify(info, null, 2), 'w');
+
+		this.echo(JSON.stringify(info, null, 2));
+		});
 	});
-	
-	var elements = this.getElementsInfo('td.lblFld');
-	for (var i = 0; i < elements.length; i++) {
-	    keyArr.push(elements[i].text);
-	}
-	
-	this.echo('Getting details');
-	var info = initialInfo(keyArr, htmlArr, nricArr);
-	
-	fs.write('results.json', JSON.stringify(info, null, 2), 'w');
-	
-	this.echo('Done!');
-	this.echo(Date.now() - start);
-    });
+	});
 });
 
 casper.run();
@@ -135,11 +183,11 @@ casper.run();
 // parser constructor functions
 
 function initialInfo(keys, values, ic) {
-    
+
     var result = {};
 
     var htmlId = 1; // counter for the ID <a.searchlink> array
-    
+
     // we get the initial general information first
 
     for (var i = 0; i < keys.length; i++) {
@@ -186,17 +234,16 @@ function capStructure(keys, values) {
 function getDirectors(values, ic, id) {
     var directors = [];
 
-    var uenReg = new RegExp(casper.cli.args[2], 'gi');
+    var idReg = /[A-Za-z]\d{7}[A-za-z]|\d{9}[A-za-z]|[A-za-z]{2}\d{7}|\d{9}/; // matches ICs and UENs
+
     for (var i = 0; i < values.length; i++) {
 
-	// conditions for start of director block:
-	// has a.searchlink
-	// next value is not empty
-	// no mention of shares or empty shareholder block
-	
-	if (/searchlink/.test(values[i]) && values[i+1].length > 0 && !(values[j+3] == 'ORDINARY' || values[j+3] == 'PREFERENCE' || values[j] == 'PLEASE REFER TO DOCUMENTARY RECORDS FOR INFORMATION')) {
-	    for (var j = i; j < values.length; j += 4) {
-		if (values[j+3] == 'ORDINARY' || values[j+3] == 'PREFERENCE' || values[j] == 'PLEASE REFER TO DOCUMENTARY RECORDS FOR INFORMATION') {
+	// does the previous cell have a date/is empty and does the next cell contain an IC or UEN? If so, this is the start of the director's block
+
+	if (!(/[A-Za-z]/.test(values[i])) && idReg.test(values[i+1])) {
+
+	    for (var j = i + 1; j < values.length; j += 4) {
+		if (values[j+3] == 'ORDINARY' || values[j+3] == 'PREFERENCE') {
 		    break;
 		};
 		var newDirector = {
@@ -217,7 +264,7 @@ function getDirectors(values, ic, id) {
 // shareholders have six table cells
 
 function getShareholders(values, ic, id) {
-    
+
     var shareholders = [];
 
     for (var i = 0; i < values.length; i++) {
@@ -225,7 +272,7 @@ function getShareholders(values, ic, id) {
 	// check start and end of shareholder block
 
 	if (values[i+3] == 'ORDINARY' || values[i+3] == 'PREFERENCE') {
-	    
+
 	    for (var j = i; j < values.length; j += 6) {
 		if (values[j+3] == undefined) {
 		    break;
@@ -243,12 +290,7 @@ function getShareholders(values, ic, id) {
 		id++;
 	    }
 	    break;
-	} else if (values[i] == 'PLEASE REFER TO DOCUMENTARY RECORDS FOR INFORMATION') {
-	    var newShareholder = {
-		data: 'PLEASE REFER TO DOCUMENTARY RECORDS FOR INFORMATION'
-	    }
-	    shareholders.push(newShareholder);
-	};
+	}
     }
     return shareholders;
 }
